384937289c27b9c6906735fec40d69a227745233
Chris Shorter <shorte24@gmail.com>
2022-02-02 17:56:43 -0500

    Creates GameSaveWriter and PlayerWriter

---
+++ PowerUp/PowerUp/GameSave/GameSaveWriter.cs
@@ -0,0 +1,51 @@
+﻿using PowerUp.DebugUtils;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace PowerUp.GameSave
+{
+  public class GameSaveWriter : IDisposable
+  {
+    private readonly Stream _stream;
+
+    public GameSaveWriter(string filePath)
+    {
+      _stream = new FileStream(filePath, FileMode.Open, FileAccess.ReadWrite);
+    }
+
+    public void WriteString(long offset, string @string)
+    {
+      
+    }
+
+    public void WriteUInt(long offset, int bitOffset, int numberOfBits, ushort @uint)
+    {
+      _stream.Seek(offset, SeekOrigin.Begin);
+      var reader = new BinaryReader(_stream);
+      var writer = new BinaryWriter(_stream);
+      var valueBits = @uint.ToBitArray(numberOfBits);
+
+      int bitsWritten = 0;
+      int bitsOfCurrentByte = bitOffset;
+      byte currentByte = reader.ReadByte();
+      while(bitsWritten < numberOfBits)
+      {
+        if(bitsOfCurrentByte >= BinaryUtils.BYTE_LENGTH)
+        {
+          writer.Write(currentByte);
+          currentByte = reader.ReadByte();
+          bitsOfCurrentByte = 0;
+        }
+
+        currentByte.SetBit(bitsOfCurrentByte, valueBits[bitsWritten]);
+
+        bitsWritten++;
+        bitsOfCurrentByte++;
+      }
+    }
+
+    public void Dispose() => _stream.Dispose();
+  }
+}
---
+++ PowerUp/PowerUp/GameSave/PlayerWriter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PowerUp.GameSave
+{
+  public class PlayerWriter : IDisposable
+  {
+    private readonly GameSaveWriter _writer;
+
+    public PlayerWriter(string fileName)
+    {
+      _writer = new GameSaveWriter(fileName);
+    }
+
+    public void Write(int powerProsId, GSPlayer player)
+    {
+      var playerOffset = OffsetUtils.GetPlayerOffset(powerProsId);
+      foreach(var property in typeof(GSPlayer).GetProperties())
+      {
+        var gameSaveAttribute = property.GetGSAttribute();
+        var propertyValue = property.GetValue(player);
+        if (gameSaveAttribute == null || propertyValue == null)
+          continue;
+
+        if (gameSaveAttribute is GSUIntAttribute uintAttr)
+          _writer.WriteUInt(playerOffset + uintAttr.Offset, uintAttr.BitOffset, uintAttr.Bits, (ushort)propertyValue);
+      }
+    }
+
+    public void Dispose() => _writer.Dispose();
+  }
+}
