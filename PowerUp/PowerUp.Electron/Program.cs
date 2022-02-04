f0ad6f8dcf17a13c94243c0427e0c98f91b8c44e
Chris Shorter <shorte24@gmail.com>
2022-02-04 18:21:49 -0500

    Renames PowerUp.Electron to PowerUp.ElectronUI and configures application to start

--- PowerUp/PowerUp.Electron/Program.cs
+++ PowerUp/PowerUp.Electron/Program.cs
@@ -1,6 +1,20 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using ElectronNET.API;
 
-app.MapGet("/", () => "Hello World!");
+namespace PowerUp.ElectronUI
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      CreateHostBuilder(args).Build().Run();
+    }
 
-app.Run();
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+      Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(webBuilder =>
+        {
+          webBuilder.UseElectron(args);
+          webBuilder.UseStartup<Startup>();
+        });
+  }
+}
