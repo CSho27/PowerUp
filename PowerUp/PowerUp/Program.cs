a254531b7b83fabe729a2bb522417849c81f5952
Chris Shorter <shorte24@gmail.com>
2022-02-03 19:03:42 -0500

    Sets Up ElectronCGI in DotNetProject

--- PowerUp/PowerUp/Program.cs
+++ PowerUp/PowerUp/Program.cs
@@ -1,4 +1,5 @@
-﻿using PowerUp.DebugUtils;
+﻿using ElectronCgi.DotNet;
+using PowerUp.DebugUtils;
 using PowerUp.GameSave;
 using System;
 
@@ -6,20 +7,47 @@ namespace PowerUp
 {
   class Program
   {
+    private const RunType RUN_TYPE = RunType.Application;
     private const string GAME_SAVE_PATH = "C:/Users/short/OneDrive/Documents/Dolphin Emulator/Wii/title/00010000/524d5045/data/pm2maus.dat";
     private const int PLAYER_ID = 20;
 
     static void Main(string[] args)
     {
-      while (true) 
+      if (RUN_TYPE == RunType.Application)
       {
-        Console.ReadLine(); 
-        using var loader = new PlayerReader(GAME_SAVE_PATH);
-        var player = loader.Read(PLAYER_ID);
-        var bitString = player.AccessoriesBytes!.ToBitString();
-        var currentTime = DateTime.Now;
-        Console.WriteLine($"Update {currentTime.ToShortDateString()} {currentTime.ToShortTimeString()}: {bitString}");
+        var connection = new ConnectionBuilder()
+          .WithLogging()
+          .Build();
+
+        // expects a request named "greeting" with a string argument and returns a string
+        connection.On("greeting", (string name) =>
+        {
+          return $"Hello {name}!";
+        });
+
+        // wait for incoming requests
+        connection.Listen();
+      } 
+      else if(RUN_TYPE == RunType.Analysis)
+      {
+        while (true) 
+        {
+          Console.ReadLine(); 
+          using var loader = new PlayerReader(GAME_SAVE_PATH);
+          var player = loader.Read(PLAYER_ID);
+          var bitString = player.AccessoriesBytes!.ToBitString();
+          var currentTime = DateTime.Now;
+          Console.WriteLine($"Update {currentTime.ToShortDateString()} {currentTime.ToShortTimeString()}: {bitString}");
+        }
       }
+      
+
     }
+  }
+
+  public enum RunType
+  {
+    Analysis,
+    Application
   }
 }
