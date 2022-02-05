50ac3d76d7fe0f64c47dd747b930d2c0b41ecb55
Chris Shorter <shorte24@gmail.com>
2022-02-05 07:03:51 -0500

    Completes proof of concept by reading saved name from power pros file

--- PowerUp/PowerUp.ElectronUI/Controllers/HomeController.cs
+++ PowerUp/PowerUp.ElectronUI/Controllers/HomeController.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PowerUp.GameSave;
 
 namespace PowerUp.ElectronUI.Controllers
 {
   public class HomeController : Controller
   {
+    private const string GAME_SAVE_PATH = "C:/Users/short/OneDrive/Documents/Dolphin Emulator/Wii/title/00010000/524d5045/data/pm2maus.dat";
+    private const int PLAYER_ID = 20;
+
     public IActionResult Index()
     {
       return base.PhysicalFile(ProjectPath.Relative("electron/dist/index.html"), "text/html");
@@ -12,7 +16,9 @@ namespace PowerUp.ElectronUI.Controllers
     [Route("/Test")]
     public JsonResult Test()
     {
-      return new JsonResult(new { Result = "Great Success!" });
+      using var loader = new PlayerReader(GAME_SAVE_PATH);
+      var player = loader.Read(PLAYER_ID);
+      return new JsonResult(new { Result = player.SavedName });
     }
   }
 
