e1489c468566f295071c97b4999e20b72ec32462
Chris Shorter <shorte24@gmail.com>
2022-02-13 12:43:30 -0500

    Creates SolutionPath which allows the loading of data from within the solution

---
+++ PowerUp/PowerUp.Tests/SolutionPathTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using Shouldly;
+using System.IO;
+
+namespace PowerUp.Tests
+{
+  public class SolutionPathTests
+  {
+    [Test] 
+    public void Root_ShouldGetTheRootOfTheSolution()
+    {
+      var projectRoot = SolutionPath.Root();
+      projectRoot.ShouldBePath("C:/dev/PowerUp/PowerUp");
+    }
+
+    [Test]
+    public void Relative_ShouldEvaluateRelativePath()
+    {
+      var path = SolutionPath.Relative("./Players/Cleveland/2003/TravisHafner");
+      path.ShouldBePath("C:/dev/PowerUp/PowerUp/Players/Cleveland/2003/TravisHafner");
+    }
+  }
+
+  public static class ShouldlyPathExtensions
+  {
+    public static void ShouldBePath(this string path1, string path2)
+      => Path.GetFullPath(path1.TrimSlashes()).ShouldBe(Path.GetFullPath(path2.TrimSlashes()));
+
+    private static string TrimSlashes(this string path) => path.TrimEnd('/', '\\');
+  }
+}
