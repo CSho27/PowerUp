558c40a3a7127d25b2e3083d3dc97adcebac1b74
Chris Shorter <shorte24@gmail.com>
2022-02-19 11:43:09 -0500

    Moves Team into its own folder

--- PowerUp/PowerUp/Entities/Teams/Team.cs
+++ PowerUp/PowerUp/Entities/Teams/Team.cs
@@ -2,13 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace PowerUp.Entities
+namespace PowerUp.Entities.Teams
 {
   public class Team
   {
     public string Name { get; private set; } = string.Empty;
     public int Year { get; private set; }
-    public bool IsCustom { get; private set; }
     public IEnumerable<Player> Players { get; private set; } = Enumerable.Empty<Player>();
   }
 }
