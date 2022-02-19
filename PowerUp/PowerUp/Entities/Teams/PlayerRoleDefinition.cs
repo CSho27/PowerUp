191ebb3dd02e20d3673812f5d0e7fa7ba618ed92
Chris Shorter <shorte24@gmail.com>
2022-02-19 11:58:03 -0500

    Creates Team Entity

---
+++ PowerUp/PowerUp/Entities/Teams/PlayerRoleDefinition.cs
@@ -0,0 +1,38 @@
+﻿using PowerUp.Entities.Players;
+
+namespace PowerUp.Entities.Teams
+{
+  public class PlayerRoleDefinition
+  {
+    public PlayerDatabaseKeys PlayerKey { get; set; }
+    public bool IsAAA { get; set; }
+    public bool IsPinchHitter { get; set; }
+    public bool IsPinchRunner { get; set; }
+    public bool IsDefensiveReplacement { get; set; }
+    public bool IsDefensiveLiability { get; set; }
+
+    public PitcherRole PitcherRole { get; set; }
+
+    public PlayerRoleDefinition(PlayerDatabaseKeys playerKey)
+    {
+      PlayerKey = playerKey;
+    }
+  }
+
+  public enum PitcherRole
+  {
+    Starter,
+    [DisplayName("Swing Man")]
+    SwingMan,
+    [DisplayName("Long Reliever")]
+    LongReliever,
+    [DisplayName("Middle Reliever")]
+    MiddleReliever,
+    [DisplayName("Situational Lefty")]
+    SituationalLefty,
+    [DisplayName("Mop-up Man")]
+    MopUpMan,
+    [DisplayName("Setup Man")]
+    SetupMan
+  }
+}
--- PowerUp/PowerUp/Entities/Teams/Team.cs
+++ PowerUp/PowerUp/Entities/Teams/Team.cs
@@ -6,8 +6,18 @@ namespace PowerUp.Entities.Teams
 {
   public class Team
   {
-    public string Name { get; private set; } = string.Empty;
-    public int Year { get; private set; }
-    public IEnumerable<Player> Players { get; private set; } = Enumerable.Empty<Player>();
+    public EntitySourceType SourceType { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int? FranchiseId { get; set; }
+    public int? Year { get; set; }
+    public string? ImportSource { get; set; }
+
+    public IEnumerable<PlayerDatabaseKeys> PlayerKeys { get; set; } = Enumerable.Empty<PlayerDatabaseKeys>();
+    
+    public IEnumerable<PlayerRoleDefinition> PlayerRoles { get; set; } = Enumerable.Empty<PlayerRoleDefinition>();
+
+    public IEnumerable<(PlayerDatabaseKeys playerKeys, Position position)> NoDHLineup { get; set; } = Enumerable.Empty<(PlayerDatabaseKeys, Position)>();
+    public IEnumerable<(PlayerDatabaseKeys playerKeys, Position position)> DHLineup { get; set; } = Enumerable.Empty<(PlayerDatabaseKeys, Position)>();
+
   }
 }
