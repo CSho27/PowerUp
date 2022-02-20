fb375a3f1a551f0a8f0e5ab4f108c6cd2a522cfc
Chris Shorter <shorte24@gmail.com>
2022-02-19 21:24:55 -0500

    Completes rename

---
+++ PowerUp/PowerUp/GameSave/IO/GameSaveFileReader.cs
@@ -0,0 +1,84 @@
+﻿using PowerUp.DebugUtils;
+using PowerUp.Libraries;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace PowerUp.GameSave.IO
+{
+  public class GameSaveFileReader : IDisposable
+  {
+    private readonly Stream _stream;
+    private readonly ICharacterLibrary _characterLibrary;
+
+    public GameSaveFileReader(ICharacterLibrary characterLibrary, string filePath)
+    {
+      _stream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+      _characterLibrary = characterLibrary;
+    }
+
+    public byte[] ReadBytes(long offset, int numberOfBytes)
+    {
+      var reader = GetReaderFor(offset);
+      var bytes = Enumerable.Empty<byte>();
+      for (int i = 0; i < numberOfBytes; i++)
+        bytes = bytes.Append(reader.ReadByte());
+
+      return bytes.ToArray();
+    }
+
+    public string ReadString(long offset, int stringLength)
+    {
+      var chars = Enumerable.Empty<char>();
+      for (int i = 0; i < stringLength; i++)
+        chars = chars.Append(ReadChar(offset + 2 * i));
+
+      return new string(chars.ToArray()).TrimEnd();
+    }
+
+    public ushort ReadUInt(long offset, int bitOffset, int numberOfBits)
+    {
+      var reader = GetReaderFor(offset);
+      var valueBits = new byte[numberOfBits];
+
+      int bitsRead = 0;
+      int bitOfCurrentByte = bitOffset;
+      byte currentByte = reader.ReadByte();
+      while (bitsRead < numberOfBits)
+      {
+        if (bitOfCurrentByte >= BinaryUtils.BYTE_LENGTH)
+        {
+          currentByte = reader.ReadByte();
+          bitOfCurrentByte = 0;
+        }
+
+        valueBits[bitsRead] = currentByte.GetBit(bitOfCurrentByte);
+
+        bitsRead++;
+        bitOfCurrentByte++;
+      }
+
+      return valueBits.ToUInt16();
+    }
+
+    public short ReadSInt(long offset, int bitOffset, int numberOfBits)
+    {
+      var isNegative = ReadBool(offset, bitOffset);
+      var value = ReadUInt(offset, bitOffset + 1, numberOfBits - 1);
+      return isNegative
+        ? (short)(value * -1)
+        : (short)value;
+    }
+
+    public char ReadChar(long offset) => _characterLibrary[ReadUInt(offset, 0, 16)];
+    public bool ReadBool(long offset, int bitOffset) => ReadUInt(offset, bitOffset, 1) == 1;
+
+    private BinaryReader GetReaderFor(long offset)
+    {
+      _stream.Seek(offset, SeekOrigin.Begin);
+      return new BinaryReader(_stream);
+    }
+
+    public void Dispose() => _stream.Dispose();
+  }
+}
---
+++ PowerUp/PowerUp/GameSave/IO/GameSaveFileWriter.cs
@@ -0,0 +1,72 @@
+﻿using PowerUp.DebugUtils;
+using PowerUp.Libraries;
+using System;
+using System.IO;
+
+namespace PowerUp.GameSave.IO
+{
+  public class GameSaveFileWriter : IDisposable
+  {
+    private readonly Stream _stream;
+    private readonly ICharacterLibrary _characterLibrary;
+
+    public GameSaveFileWriter(ICharacterLibrary characterLibrary, string filePath)
+    {
+      _stream = new FileStream(filePath, FileMode.Open, FileAccess.ReadWrite);
+      _characterLibrary = characterLibrary;
+    }
+
+    public void WriteString(long offset, int stringLength, string @string)
+    {
+      if (@string.Length > stringLength)
+        throw new ArgumentException("String longer than specified length", nameof(@string));
+
+      for (int i = 0; i < stringLength; i++)
+        WriteChar(offset + 2 * i, i < @string.Length ? @string[i] : ' ');
+    }
+
+    public void WriteUInt(long offset, int bitOffset, int numberOfBits, ushort @uint)
+    {
+      _stream.Seek(offset, SeekOrigin.Begin);
+      var writer = new PeekingBinaryWriter(_stream);
+      var valueBits = @uint.ToBitArray(numberOfBits);
+
+      int bitsWritten = 0;
+      int bitsOfCurrentByte = bitOffset;
+      byte currentByte = writer.PeekByte();
+      while (bitsWritten < numberOfBits)
+      {
+        if (bitsOfCurrentByte >= BinaryUtils.BYTE_LENGTH)
+        {
+          writer.Write(currentByte);
+          currentByte = writer.PeekByte();
+          bitsOfCurrentByte = 0;
+        }
+
+        currentByte = currentByte.SetBit(bitsOfCurrentByte, valueBits[bitsWritten]);
+
+        bitsWritten++;
+        bitsOfCurrentByte++;
+      }
+      writer.Write(currentByte);
+    }
+
+    public void WriteSInt(long offset, int bitOffset, int numberOfBits, short sint)
+    {
+      var isNegative = sint < 0;
+      WriteBool(offset, bitOffset, isNegative);
+      WriteUInt(offset, bitOffset + 1, numberOfBits - 1, (ushort)Math.Abs(sint));
+    }
+
+    public void WriteChar(long offset, char @char)
+    {
+      var charNum = _characterLibrary[@char];
+      if (charNum.HasValue)
+        WriteUInt(offset, 0, 16, charNum.Value);
+    }
+
+    public void WriteBool(long offset, int bitOffset, bool @bool) => WriteUInt(offset, bitOffset, 1, (ushort)(@bool ? 1 : 0));
+
+    public void Dispose() => _stream.Dispose();
+  }
+}
