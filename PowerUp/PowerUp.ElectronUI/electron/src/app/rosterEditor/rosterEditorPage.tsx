5d039c7770fc63a7c2d8c43d0fa446290b15640c
Chris Shorter <shorte24@gmail.com>
2022-02-20 15:22:35 -0500

    Creates Breadcrumb component

--- PowerUp/PowerUp.ElectronUI/electron/src/app/rosterEditor/rosterEditorPage.tsx
+++ PowerUp/PowerUp.ElectronUI/electron/src/app/rosterEditor/rosterEditorPage.tsx
@@ -1,3 +1,4 @@
+import { Breadcrumbs, Crumb } from "../../components/breadcrumbs/breadbrumbs";
 import { AppContext } from "../app";
 import { PowerUpLayout } from "../shared/powerUpLayout";
 
@@ -10,6 +11,14 @@ export function RosterEditorPage(props: RosterEditorPageProps) {
   const { appContext, teamNames } = props;
   
   return <PowerUpLayout headerText='Edit Roster'>
+    <Breadcrumbs>
+      <Crumb key={1} onClick={() => console.log(1)}>1</Crumb>
+      <Crumb key={2} onClick={() => console.log(2)}>2</Crumb>
+      <Crumb key={3} onClick={() => console.log(3)}>3</Crumb>
+      <Crumb key={4} onClick={() => console.log(4)}>4</Crumb>
+      <Crumb key={5} onClick={() => console.log(5)}>5</Crumb>
+      <Crumb key={6} >6</Crumb>
+    </Breadcrumbs>
     {teamNames.map(t => <div key={t}>{t}</div>)}
   </PowerUpLayout>
 }
---
+++ PowerUp/PowerUp.ElectronUI/electron/src/components/breadcrumbs/breadbrumbs.tsx
@@ -0,0 +1,62 @@
+import { ReactElement } from "react";
+import styled from "styled-components";
+import { COLORS, FONT_SIZES } from "../../style/constants";
+
+export interface BreadcrumbsProps {
+  children: ReactElement<CrumbProps>[];
+}
+
+
+export function Breadcrumbs(props: BreadcrumbsProps) {
+  return <nav aria-label="Breadcrumb">
+    <BreadcrumbList>{props.children.map(toCrumb)}</BreadcrumbList>
+  </nav>
+
+  function toCrumb(child: ReactElement<CrumbProps>) {
+    return <Wrapper key={child.key}>{child}</Wrapper>;
+  }
+}
+
+const BreadcrumbList = styled.ol`
+  padding: 0;
+  margin: 0;
+  list-style-type: none;
+  color: ${COLORS.primaryBlue.regular_45};
+  font-weight: bold;
+  font-size: ${FONT_SIZES._24};
+`
+
+const Wrapper = styled.li`
+  display: inline;
+
+  &:not(:first-of-type){
+    &:before {
+      font-weight: 900;
+      content: '>';
+    }
+  }
+`
+
+export interface CrumbProps {
+  children?: React.ReactNode;
+  onClick?: () => void;
+}
+
+export function Crumb(props: CrumbProps) {
+  return <CrumbLink onClick={props.onClick}>
+    {props.children}
+  </CrumbLink>;
+}
+
+const CrumbLink = styled.a`
+  text-decoration: none;
+  padding: 4px;
+ 
+  ${p => p.onClick
+    ? `&:hover {
+        text-decoration: underline;
+        cursor: pointer;
+      }`
+    : undefined
+  }
+`
