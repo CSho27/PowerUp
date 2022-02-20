6fc7683a5369ce8afbeeda16a13d9482508aba1d
Chris Shorter <shorte24@gmail.com>
2022-02-19 21:32:06 -0500

    Adds GameSaveReader, which loads all data for a game save file

---
+++ PowerUp/PowerUp/GameSave/Objects/GameSaves/GameSave.cs
@@ -0,0 +1,15 @@
+﻿using PowerUp.GameSave.Objects.Lineups;
+using PowerUp.GameSave.Objects.Players;
+using PowerUp.GameSave.Objects.Teams;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PowerUp.GameSave.Objects.GameSaves
+{
+  public class GameSave
+  {
+    public IEnumerable<GSPlayer> Players { get; set; } = Enumerable.Empty<GSPlayer>();
+    public IEnumerable<GSTeam> Teams { get; set; } = Enumerable.Empty<GSTeam>();
+    public IEnumerable<GSLineupDefinition> Lineups { get; set; } = Enumerable.Empty<GSLineupDefinition>();
+  }
+}
---
+++ PowerUp/PowerUp/GameSave/Objects/GameSaves/GameSaveReader.cs
@@ -0,0 +1,46 @@
+﻿using PowerUp.GameSave.IO;
+using PowerUp.GameSave.Objects.Lineups;
+using PowerUp.GameSave.Objects.Players;
+using PowerUp.GameSave.Objects.Teams;
+using PowerUp.Libraries;
+using System;
+using System.Collections.Generic;
+
+namespace PowerUp.GameSave.Objects.GameSaves
+{
+  public class GameSaveReader : IDisposable
+  {
+    private readonly GameSaveObjectReader _reader;
+
+    public GameSaveReader(ICharacterLibrary characterLibrary, string fileName)
+    {
+      _reader = new GameSaveObjectReader(characterLibrary, fileName);
+    }
+
+    /// <summary>
+    /// Reads GameSave file into data objects
+    /// </summary>
+    /// <returns></returns>
+    public GameSave Read()
+    {
+      var playerReader = new PlayerReader(_reader);
+      var gsPlayers = new List<GSPlayer>();
+      for (int i = 1; i < 1500; i++)
+        gsPlayers.Add(playerReader.Read(i));
+
+      var teamReader = new TeamReader(_reader);
+      var lineupReader = new LineupReader(_reader);
+      var gsTeams = new List<GSTeam>();
+      var gsLineups = new List<GSLineupDefinition>();
+      for (int i = 0; i < 32; i++)
+      {
+        gsTeams.Add(teamReader.Read(i));
+        gsLineups.Add(lineupReader.Read(i));
+      }
+
+      return new GameSave { Players = gsPlayers, Teams = gsTeams, Lineups = gsLineups };  
+    }
+
+    public void Dispose() => _reader.Dispose();
+  }
+}
