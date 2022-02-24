fa465af6fe10891577c03eb981e2315483e34329
Chris Shorter <shorte24@gmail.com>
2022-02-23 23:58:38 -0500

    Creates and registers SpecialSavedNameLibrary

--- PowerUp/PowerUp/Libraries/LibraryConfig.cs
+++ PowerUp/PowerUp/Libraries/LibraryConfig.cs
@@ -8,6 +8,7 @@ namespace PowerUp.Libraries
     public static void RegisterLibraries(this IServiceCollection services, string dataDirectory)
     {
       services.AddSingleton<ICharacterLibrary>(provider => new CharacterLibrary(Path.Combine(dataDirectory, "./data/Character_Library.csv")));
+      services.AddSingleton<ISpecialSavedNameLibrary>(provider => new SpecialSavedNameLibrary(Path.Combine(dataDirectory, "./data/SpecialSavedName_Library.csv")));
       services.AddTransient<IBaseGameSavePathProvider>(provider => new BaseGameSavePathProvider(Path.Combine(dataDirectory, "./data/BASE.pm2maus.dat")));
     }
 
---
+++ PowerUp/PowerUp/Libraries/SpecialSavedNameLibrary.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace PowerUp.Libraries
+{
+  public interface ISpecialSavedNameLibrary
+  {
+    int this[string key] { get; }
+    string this[int key] { get; }
+  }
+
+  public class SpecialSavedNameLibrary : CsvKeyValueLibrary<string, int>, ISpecialSavedNameLibrary
+  {
+    public SpecialSavedNameLibrary(string libraryFilePath): base(libraryFilePath) { }
+
+    protected override int OnKeyNotFound(string key) => throw new KeyNotFoundException();
+    protected override string OnValueNotFound(int value) => throw new KeyNotFoundException();
+
+    protected override string ParseKey(string keyString) => keyString;
+    protected override int ParseValue(string valueString) => int.Parse(valueString);
+  }
+}
