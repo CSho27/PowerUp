2a28ee35ea84a635c87a060dde85dc5f6b16479a
Chris Shorter <shorte24@gmail.com>
2022-03-02 19:26:43 -0500

    Creates ultra simple validation class

---
+++ PowerUp/PowerUp/Validation/Validator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PowerUp.Validation
+{
+  public abstract class Validator<TParameters>
+  {
+    public abstract void Validate(TParameters parameters);
+
+    protected void ThrowIfNull(object? value)
+    {
+      if (value == null)
+        throw new ArgumentNullException(nameof(value));
+    }
+
+    protected void ThrowIfNullOrEmpty(string? value)
+    {
+      if(string.IsNullOrEmpty(value))
+        throw new ArgumentOutOfRangeException(nameof(value));
+    }
+
+    protected void ThrowIfLongerThanMaxLength(string? value, int maxLength)
+    {
+      if (value != null && value.Length > maxLength)
+        throw new ArgumentOutOfRangeException(nameof(value));
+    }
+  }
+}
