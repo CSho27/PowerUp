f70eea4adf2a762f6ffcdd4ba2af1a43c7ad5cbd
Chris Shorter <shorte24@gmail.com>
2022-03-25 17:39:38 -0400

    Rearranges commands into alphabetical order

--- PowerUp/PowerUp.ElectronUI/StartupConfig/CommandConfiguration.cs
+++ PowerUp/PowerUp.ElectronUI/StartupConfig/CommandConfiguration.cs
@@ -8,23 +8,25 @@ namespace PowerUp.ElectronUI.StartupConfig
     public static void RegisterCommandsForDI(this IServiceCollection services)
     {
       services.AddSingleton(serviceProvider => new CommandRegistry(type => serviceProvider.GetRequiredService(type)));
-      services.AddSingleton<SavePlayerCommand>();
+
+      services.AddSingleton<ExportRosterCommand>();
       services.AddSingleton<LoadBaseGameSaveCommand>();
-      services.AddSingleton<LoadPlayerEditorCommand>();
       services.AddSingleton<LoadExistingRosterCommand>();
       services.AddSingleton<LoadExistingRosterOptionsCommand>();
-      services.AddSingleton<ExportRosterCommand>();
+      services.AddSingleton<LoadPlayerEditorCommand>();
+      services.AddSingleton<SavePlayerCommand>();
     }
      
     public static void AddCommandsToRegistry(this IServiceProvider serviceProvider)
     {
       var commandRegistry = serviceProvider.GetRequiredService<CommandRegistry>();
-      commandRegistry.RegisterCommand(typeof(SavePlayerCommand), "SavePlayer");
+
+      commandRegistry.RegisterCommand(typeof(ExportRosterCommand), "ExportRoster");
       commandRegistry.RegisterCommand(typeof(LoadBaseGameSaveCommand), "LoadBaseGameSave");
-      commandRegistry.RegisterCommand(typeof(LoadPlayerEditorCommand), "LoadPlayerEditor");
       commandRegistry.RegisterCommand(typeof(LoadExistingRosterCommand), "LoadExistingRoster");
       commandRegistry.RegisterCommand(typeof(LoadExistingRosterOptionsCommand), "LoadExistingRosterOptions");
-      commandRegistry.RegisterCommand(typeof(ExportRosterCommand), "ExportRoster");
+      commandRegistry.RegisterCommand(typeof(LoadPlayerEditorCommand), "LoadPlayerEditor");
+      commandRegistry.RegisterCommand(typeof(SavePlayerCommand), "SavePlayer");
     }
   }
 }
