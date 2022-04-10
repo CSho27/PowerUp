4a4cde5f7ba9acf60c27e35f5dc65b2badb03bb0
Chris Shorter <shorte24@gmail.com>
2022-04-09 23:29:35 -0400

    Adds enum types for hair/facial hair styles and colors and eyewear styles

--- PowerUp/PowerUp/Entities/Players/Appearance.cs
+++ PowerUp/PowerUp/Entities/Players/Appearance.cs
@@ -15,6 +15,140 @@
   }
   public enum EyeColor { Brown, Blue }
   public enum EyebrowThickness { Thick, Thin }
+
+  public enum HairStyle
+  {
+    [DisplayName("Flat (Medium)")]
+    FlatMedium = 1,
+    [DisplayName("Flat (Long)")]
+    FlatLong,
+    [DisplayName("Flat (Short)")]
+    FlatShort,
+    [DisplayName("Flow (Medium)")]
+    FlowMedium,
+    [DisplayName("Flow (Long)")]
+    FlowLong,
+    Bob,
+    Drealocks,
+    Afro,
+    [DisplayName("Curly (Medium)")]
+    CurlyMedium,
+    [DisplayName("Curly (Short)")]
+    CurlyShort,
+    [DisplayName("Curly Flare (X Long)")]
+    CurlyFlareExtraLong,
+    [DisplayName("Curly (Long)")]
+    CurlyLong,
+    [DisplayName("Curly Flare w/ Side Burns (X Long)")]
+    CurlyFlareWithSideburnsExtraLong,
+    [DisplayName("Curly (X Long)")]
+    CurlyExtraLong,
+    Basic,
+    [DisplayName("Basic w/ Short Sideburns")]
+    BasicShortSideburns,
+    [DisplayName("Basic w/ Medium Sideburns")]
+    BasicMediumSideburns,
+    [DisplayName("Basic w/ Long Sideburns")]
+    BasicLongSideburns,
+    [DisplayName("Fade")]
+    Fade,
+    [DisplayName("Fade w/ Short Sideburns")]
+    FadeShortSideburns,
+    [DisplayName("Fade w/ Medium Sideburns")]
+    FadeMediumSideburns
+  }
+
+  public enum FacialHairStyle
+  {
+    [DisplayName("Lampshade Mustache")]
+    LampshadeMustache = 1,
+    [DisplayName("Thin Mustache")]
+    ThinMustache,
+    [DisplayName("Fu Manchu")]
+    FuManchu,
+    [DisplayName("Pencil Mustache w/ Gap")]
+    PencilMustacheWithGap,
+    [DisplayName("Pencil Mustache")]
+    PencilMustache,
+    [DisplayName("Soul Patch")]
+    SoulPatch,
+    [DisplayName("Petite Goatee")]
+    PetiteGoatee,
+    [DisplayName("Thin Petite Goatee")]
+    ThinPetiteGoatee,
+    [DisplayName("Anchor Goatee")]
+    AnchorGoatee,
+    [DisplayName("Goatee")]
+    Goatee,
+    [DisplayName("Rounded Goatee")]
+    RoundedGoatee,
+    [DisplayName("Goat Patch")]
+    GoatPatch,
+    [DisplayName("Soul Patch Goatee")]
+    SoulPatchGoatee,
+    [DisplayName("Thick Circle")]
+    ThickCircle,
+    [DisplayName("Circle")]
+    Circle,
+    [DisplayName("Thin Circle")]
+    ThinCircle,
+    [DisplayName("Full")]
+    Full,
+    [DisplayName("Van Dyke")]
+    VanDyke,
+    [DisplayName("Disconnected Circle")]
+    DisconnectedCircle,
+    [DisplayName("Chinstrap w/ Mustache")]
+    ChinstrapWithMustache,
+    [DisplayName("Chinstrap")]
+    Chinstrap,
+    [DisplayName("Thick Chinstrap")]
+    ThickChinstrap,
+    [DisplayName("Klingon Goatee")]
+    KlingonGoatee,
+    [DisplayName("Klingon")]
+    Klingon,
+    [DisplayName("Thick Klingon")]
+    ThickKlingon,
+    [DisplayName("Thick Full")]
+    ThickFull,
+    [DisplayName("Thick Full X Long")]
+    ThickFullExtraLong,
+    [DisplayName("Mutton Chops")]
+    MuttonChops
+  }
+
+  public enum HairColor
+  {
+    [DisplayName("Light Blonde")]
+    LightBlonde,
+    [DisplayName("Blonde")]
+    Blonde,
+    [DisplayName("Dark Blonde")]
+    DarkBlonde,
+    [DisplayName("Dark Brown")]
+    DarkBrown,
+    [DisplayName("Very Light Brown")]
+    VeryLightBrown,
+    [DisplayName("Very Light Brown w/ Red")]
+    VeryLightBrownWithRed,
+    [DisplayName("Light Brown")]
+    LightBrown,
+    [DisplayName("Brown")]
+    Brown,
+    [DisplayName("Black")]
+    Black,
+    [DisplayName("Gray")]
+    Gray,
+    [DisplayName("Yellow")]
+    Yellow,
+    [DisplayName("Red")]
+    Red,
+    [DisplayName("Blue")]
+    Blue,
+    [DisplayName("Green")]
+    Green
+  }
   
   public enum BatColor 
   { 
@@ -39,6 +173,34 @@
     [DisplayName("Dark Brown")]
     DarkBrown, 
     Red 
+  }
+
+  public enum EyewearStyle
+  {
+    [DisplayName("Eye Black")]
+    EyeBlack = 1,
+    [DisplayName("Rec Specs")]
+    RecSpecs,
+    [DisplayName("Rectangle Rec Specs")]
+    RectangleRecSpecs,
+    [DisplayName("Pointed Rec Specs")]
+    PointedRecSpecs,
+    [DisplayName("Oversized Rec Specs")]
+    OversizedRecSpecs,
+    Circle,
+    Oval,
+    [DisplayName("Thin Oval")]
+    ThinOval,
+    [DisplayName("Semi-Circle")]
+    SemiCircle,
+    [DisplayName("Flak Jacket")]
+    FlakJacket,
+    [DisplayName("Half Jacket")]
+    HalfJacket,
+    [DisplayName("Radar")]
+    Radar,
+    [DisplayName("Thin Radar")]
+    ThinRadar
   }
 
   public enum EyewearFrameColor
