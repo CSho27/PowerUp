b7365b01f4909e93d72059538b95dffd50df8db0
Chris Shorter <shorte24@gmail.com>
2022-04-13 18:34:21 -0400

    Creates winPack script to package application for windows

---
+++ PowerUp/PowerUp.ElectronUI/winPack.ps1
@@ -0,0 +1,4 @@
+electronize build /target win;
+Copy-Item "./Data" "./bin/Desktop/win-unpacked/resources/bin/Data" -recurse;
+Remove-Item "./bin/Desktop/win-unpacked/resources/bin/appsettings.json";
+Rename-Item -Path "./bin/Desktop/win-unpacked/resources/bin/appsettings.Release.json" -NewName "appsettings.json"
