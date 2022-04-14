678087e4d7ad0aa8028685662b67bd58f9d65e8b
Chris Shorter <shorte24@gmail.com>
2022-04-13 22:13:51 -0400

    Updates osx build script to match output folder

--- PowerUp/PowerUp.ElectronUI/macPack.ps1
+++ PowerUp/PowerUp.ElectronUI/macPack.ps1
@@ -3,6 +3,6 @@ npm install;
 npm run build;
 Set-Location ..;
 electronize build /target osx;
-Copy-Item "./Data/data" "./bin/Desktop/osx-unpacked/resources/bin/Data/data" -recurse;
-Remove-Item "./bin/Desktop/osx-unpacked/resources/bin/appsettings.json";
-Rename-Item -Path "./bin/Desktop/osx-unpacked/resources/bin/appsettings.Release.json" -NewName "appsettings.json"
+Copy-Item "./Data/data" "./bin/Desktop/osx/bin/Data/data" -recurse;
+Remove-Item "./bin/Desktop/osx/bin/appsettings.json";
+Rename-Item -Path "./bin/Desktop/osx/bin/appsettings.Release.json" -NewName "appsettings.json"
