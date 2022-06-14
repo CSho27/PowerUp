d6f471aecec907df8bcc0c18d91c16a6fb2e3eb7
Chris Shorter <shorte24@gmail.com>
2022-06-14 07:07:00 -0400

    Adds apiclient for calling and deserializing data

---
+++ PowerUp/PowerUp/Fetchers/ApiClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace PowerUp.Fetchers
+{
+  public class ApiClient
+  {
+    private readonly HttpClient _client = new HttpClient();
+
+    private async Task<string> GetContent(string url)
+    {
+      var response = await _client.GetAsync(url);
+      if(response.StatusCode != System.Net.HttpStatusCode.OK)
+        throw new Exception(response.StatusCode.ToString());
+
+      return await response.Content.ReadAsStringAsync();
+    }
+
+    public async Task<T> Get<T>(string url)
+    {
+      var content = await GetContent(url);
+      return JsonSerializer.Deserialize<T>(content)!;
+    }
+  }
+}
