fc6d93b3df0fe35e17b4dd856a6123f85358b3e5
Chris Shorter <shorte24@gmail.com>
2022-06-14 19:42:16 -0400

    Adds player search test for historic players

--- PowerUp/PowerUp.Tests/Fetchers/MLBLookupService/MLBLookupServiceTests.cs
+++ PowerUp/PowerUp.Tests/Fetchers/MLBLookupService/MLBLookupServiceTests.cs
@@ -13,7 +13,7 @@ namespace PowerUp.Tests.Fetchers.MLBLookupService
     private readonly IMLBLookupServiceClient _fetcher = new MLBLookupServiceClient();
 
     [Test]
-    public void SearchPlayer_SearchesSinglePlayer()
+    public void SearchPlayer_SearchesSingleCurrentPlayer()
     {
       Task.Run(async () =>
       {
@@ -38,6 +38,35 @@ namespace PowerUp.Tests.Fetchers.MLBLookupService
         stanton.ProDebutDate.ShouldBe(DateTime.Parse("2010-06-08T00:00:00"));
         stanton.ServiceYears.ShouldBe(null);
         stanton.IsActive.ShouldBe(true);
+      }).GetAwaiter().GetResult();
+    }
+
+    [Test]
+    public void SearchPlayer_SearchesSingleHistoricPlayer()
+    {
+      Task.Run(async () =>
+      {
+        var result = await _fetcher.SearchPlayer("Sandy Koufax");
+        result.TotalResults.ShouldBe(1);
+        var stanton = result.Results.Single();
+        stanton.PlayerId.ShouldBe(117277);
+        stanton.FirstName.ShouldBe("Sandy");
+        stanton.LastName.ShouldBe("Koufax");
+        stanton.Position.ShouldBe(Position.Pitcher);
+        stanton.BattingSide.ShouldBe(BattingSide.Right);
+        stanton.ThrowingArm.ShouldBe(ThrowingArm.Left);
+        stanton.Weight.ShouldBe(210);
+        stanton.HeightFeet.ShouldBe(6);
+        stanton.HeightInches.ShouldBe(2);
+        stanton.BirthDate.ShouldBe(DateTime.Parse("1935-12-30T00:00:00"));
+        stanton.BirthCountry.ShouldBe("USA");
+        stanton.BirthState.ShouldBe("NY");
+        stanton.BirthCity.ShouldBe("Brooklyn");
+        stanton.HighSchool.ShouldBe("Lafayette, Brooklyn, NY");
+        stanton.College.ShouldBe("Cincinnati");
+        stanton.ProDebutDate.ShouldBe(DateTime.Parse("1955-06-24T00:00:00"));
+        stanton.ServiceYears.ShouldBe(null);
+        stanton.IsActive.ShouldBe(false);
       }).GetAwaiter().GetResult();
     }
 
