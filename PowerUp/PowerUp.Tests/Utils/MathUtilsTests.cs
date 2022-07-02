f4114c9906984cb3f8a02615da6d71c14ededdf7
Chris Shorter <shorte24@gmail.com>
2022-07-02 09:30:31 -0400

    Adds piecewise function generator to math utils

---
+++ PowerUp/PowerUp.Tests/Utils/MathUtilsTests.cs
@@ -0,0 +1,170 @@
+﻿using NUnit.Framework;
+using Shouldly;
+using System;
+
+namespace PowerUp.Tests.Utils
+{
+  public class MathUtilsTests
+  {
+    [Test]
+    public void PiecewiseFunctionFor_ThrowsIfZeroCoordinates()
+    {
+      Assert.Throws<InvalidOperationException>(() =>
+      {
+        var piecewiseFunction = MathUtils.PiecewiseFunctionFor(new (double x, double y)[] { });
+        piecewiseFunction(0);
+      });
+    }
+
+    [Test]
+    public void PiecewiseFunctionFor_ThrowsIfOneCoordinate()
+    {
+      Assert.Throws<InvalidOperationException>(() =>
+      {
+        var piecewiseFunction = MathUtils.PiecewiseFunctionFor(new[] { (0.0, 0.0) });
+        piecewiseFunction(0);
+      });
+    }
+
+    [Test]
+    public void PiecewiseFunctionFor_ThrowsIfTwoCoordinatesHaveSameX()
+    {
+      var coordinates = new (double x, double y)[]
+      {
+        (0, 0),
+        (0, 1)
+      };
+
+      var piecewiseFunction = MathUtils.PiecewiseFunctionFor(coordinates);
+      Assert.Throws<InvalidOperationException>(() =>
+      {
+        var result = piecewiseFunction(0);
+      });
+    }
+
+    [Test]
+    public void PiecewiseFunctionFor_BuildsPiecewiseFunctionForTwoCoordinates()
+    {
+      var coordinates = new (double x, double y)[]
+      {
+        (0, 0),
+        (1, 1)
+      };
+
+      var piecewiseFunction = MathUtils.PiecewiseFunctionFor(coordinates);
+      var result = piecewiseFunction(0);
+      result.ShouldBe(0);
+    }
+
+    [Test]
+    public void PiecewiseFunctionFor_ShouldFollowLinePastFirstPoint()
+    {
+      var coordinates = new (double x, double y)[]
+      {
+        (0, 0),
+        (1, 1)
+      };
+
+      var piecewiseFunction = MathUtils.PiecewiseFunctionFor(coordinates);
+      var result = piecewiseFunction(-1);
+      result.ShouldBe(-1);
+    }
+
+    [Test]
+    public void PiecewiseFunctionFor_ShouldFollowLinePastLastPoint()
+    {
+      var coordinates = new (double x, double y)[]
+      {
+        (0, 0),
+        (1, 1)
+      };
+
+      var piecewiseFunction = MathUtils.PiecewiseFunctionFor(coordinates);
+      var result = piecewiseFunction(2);
+      result.ShouldBe(2);
+    }
+
+    [Test]
+    public void PiecewiseFunctionFor_BuildsPiecewiseFunctionForThreeCoordinates()
+    {
+      var coordinates = new (double x, double y)[]
+      {
+        (0, 0),
+        (1, 1),
+        (2, 1)
+      };
+
+      var piecewiseFunction = MathUtils.PiecewiseFunctionFor(coordinates);
+      piecewiseFunction(1.5).ShouldBe(1);
+      piecewiseFunction(3).ShouldBe(1);
+    }
+
+    [Test]
+    public void PiecewiseFunctionFor_BuildsPiecewiseFunctionForMultipleCoordinates()
+    {
+      var coordinates = new (double x, double y)[]
+      {
+        (0, 0),
+        (1, 1),
+        (2, 1),
+        (3, 3),
+        (4, 2),
+        (5, 1.5),
+        (6, 2.5)
+      };
+
+      var piecewiseFunction = MathUtils.PiecewiseFunctionFor(coordinates);
+      piecewiseFunction(-1).ShouldBe(-1);
+      piecewiseFunction(-.5).ShouldBe(-.5);
+      piecewiseFunction(0).ShouldBe(0);
+      piecewiseFunction(0.5).ShouldBe(0.5);
+      piecewiseFunction(1).ShouldBe(1);
+      piecewiseFunction(1.5).ShouldBe(1);
+      piecewiseFunction(2).ShouldBe(1);
+      piecewiseFunction(2.5).ShouldBe(2);
+      piecewiseFunction(3).ShouldBe(3);
+      piecewiseFunction(3.5).ShouldBe(2.5);
+      piecewiseFunction(4).ShouldBe(2);
+      piecewiseFunction(4.5).ShouldBe(1.75);
+      piecewiseFunction(5).ShouldBe(1.5);
+      piecewiseFunction(5.5).ShouldBe(2);
+      piecewiseFunction(6).ShouldBe(2.5);
+      piecewiseFunction(6.5).ShouldBe(3);
+      piecewiseFunction(7).ShouldBe(3.5);
+    }
+
+    [Test]
+    public void PiecewiseFunctionFor_BuildsPiecewiseFunctionCorrectlyWhenCoordinatesOutOfOrder()
+    {
+      var coordinates = new (double x, double y)[]
+      {
+        (2, 1),
+        (6, 2.5),
+        (3, 3),
+        (1, 1),
+        (5, 1.5),
+        (4, 2),
+        (0, 0)
+      };
+
+      var piecewiseFunction = MathUtils.PiecewiseFunctionFor(coordinates);
+      piecewiseFunction(-1).ShouldBe(-1);
+      piecewiseFunction(-.5).ShouldBe(-.5);
+      piecewiseFunction(0).ShouldBe(0);
+      piecewiseFunction(0.5).ShouldBe(0.5);
+      piecewiseFunction(1).ShouldBe(1);
+      piecewiseFunction(1.5).ShouldBe(1);
+      piecewiseFunction(2).ShouldBe(1);
+      piecewiseFunction(2.5).ShouldBe(2);
+      piecewiseFunction(3).ShouldBe(3);
+      piecewiseFunction(3.5).ShouldBe(2.5);
+      piecewiseFunction(4).ShouldBe(2);
+      piecewiseFunction(4.5).ShouldBe(1.75);
+      piecewiseFunction(5).ShouldBe(1.5);
+      piecewiseFunction(5.5).ShouldBe(2);
+      piecewiseFunction(6).ShouldBe(2.5);
+      piecewiseFunction(6.5).ShouldBe(3);
+      piecewiseFunction(7).ShouldBe(3.5);
+    }
+  }
+}
--- PowerUp/PowerUp/MathUtils.cs
+++ PowerUp/PowerUp/MathUtils.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PowerUp
 {
@@ -7,6 +9,43 @@ namespace PowerUp
     public static Func<double, double> BuildLinearGradientFunction(double max, double avg, double desiredMax, double desiredAverage)
     {
       return (double x) => desiredMax + ((desiredAverage - desiredMax) / (avg - max)) * (x - max);
+    }
+
+    // This assumes that the desired line graph continues in a straight line beyond the beyond the first and last points
+    public static Func<double, double> PiecewiseFunctionFor(IEnumerable<(double x, double y)> coordinates)
+    {
+      return (double input) => PiecewiseFunction(coordinates, input);
+    }
+
+    private static double PiecewiseFunction(IEnumerable<(double x, double y)> coordinates, double input)
+    {
+      var (coordinate1, coordinate2) = FindCoordinatesBetween(coordinates, input);
+      var slope = (coordinate2.y - coordinate1.y) / (coordinate2.x - coordinate1.x);
+      var intercept = coordinate1.y - slope * coordinate1.x;
+
+      return slope * input + intercept;
+    }
+
+    private static ((double x, double y) coordinate1, (double x, double y)  coordinate2) FindCoordinatesBetween(IEnumerable<(double x, double y)> coordinates, double input)
+    {
+      var coordinateList = coordinates.OrderBy(c => c.x).ToList();
+      if (coordinateList.Count < 2)
+        throw new InvalidOperationException("Coordinate list must contain at least 2 coordinates to generate a piecewise fuinction");
+
+      if(coordinateList.DistinctBy(c => c.x).Count() != coordinateList.Count)
+        throw new InvalidOperationException("Coordinate list cannot have two coordinates with the same x value");
+
+      var firstCoordinateGreaterThanInputIndex = coordinateList.FindIndex(c => c.x > input);
+
+      // If the first x is greater than the coordinate, use first two coordinates
+      if (firstCoordinateGreaterThanInputIndex == 0)
+        return (coordinateList[0], coordinateList[1]);
+
+      // If no x is greater than the input use the last two coordinates
+      if (firstCoordinateGreaterThanInputIndex == -1)
+        return (coordinateList[coordinateList.Count - 2], coordinateList[coordinateList.Count - 1]);
+
+      return (coordinateList[firstCoordinateGreaterThanInputIndex - 1], coordinateList[firstCoordinateGreaterThanInputIndex]);
     }
   }
 }
