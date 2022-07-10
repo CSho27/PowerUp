a54042acc0ba92e75b2cad1e4a07815960ad355d
Chris Shorter <shorte24@gmail.com>
2022-07-09 22:20:41 -0400

    Adds values for mapping pp teams

--- PowerUp/PowerUp/Entities/MLBPPTeams.cs
+++ PowerUp/PowerUp/Entities/MLBPPTeams.cs
@@ -7,69 +7,69 @@ namespace PowerUp.Entities
     /// <summary>
     /// Note: The National League All-Stars have the Id 0, but are last in the list of team definitions
     /// </summary>
-    [Abbrev("NL"), DisplayName("National League All-Stars"), Division(MLBPPDivision.AllStars)]
+    [Abbrev("NL"), DisplayName("National League All-Stars"), Division(MLBPPDivision.AllStars), LSTeamId(160)]
     NationalLeagueAllStars,
-    [Abbrev("Bal"), TeamLocation("Baltimore"), Division(MLBPPDivision.ALEast)]
+    [Abbrev("Bal"), TeamLocation("Baltimore"), Division(MLBPPDivision.ALEast), LSTeamId(110)]
     Orioles = 1,
-    [Abbrev("Bos"), TeamLocation("Boston"), DisplayName("Red Sox"), Division(MLBPPDivision.ALEast)]
+    [Abbrev("Bos"), TeamLocation("Boston"), DisplayName("Red Sox"), Division(MLBPPDivision.ALEast), LSTeamId(111)]
     RedSox,
-    [Abbrev("NYY"), TeamLocation("New York"), Division(MLBPPDivision.ALEast)]
+    [Abbrev("NYY"), TeamLocation("New York"), Division(MLBPPDivision.ALEast), LSTeamId(147)]
     Yankees,
-    [Abbrev("TB"), TeamLocation("Tampa Bay"), Division(MLBPPDivision.ALEast)]
+    [Abbrev("TB"), TeamLocation("Tampa Bay"), Division(MLBPPDivision.ALEast), LSTeamId(139)]
     Rays,
-    [Abbrev("Tor"), TeamLocation("Toronto"), DisplayName("Blue Jays"), Division(MLBPPDivision.ALEast)]
+    [Abbrev("Tor"), TeamLocation("Toronto"), DisplayName("Blue Jays"), Division(MLBPPDivision.ALEast), LSTeamId(141)]
     BlueJays,
-    [Abbrev("CHW"), TeamLocation("Chicago"), DisplayName("White Sox"), Division(MLBPPDivision.ALCentral)]
+    [Abbrev("CHW"), TeamLocation("Chicago"), DisplayName("White Sox"), Division(MLBPPDivision.ALCentral), LSTeamId(145)]
     WhiteSox,
-    [Abbrev("Cle"), TeamLocation("Cleveland"), Division(MLBPPDivision.ALCentral)]
+    [Abbrev("Cle"), TeamLocation("Cleveland"), Division(MLBPPDivision.ALCentral), LSTeamId(114)]
     Indians,
-    [Abbrev("Det"), TeamLocation("Detroit"), Division(MLBPPDivision.ALCentral)]
+    [Abbrev("Det"), TeamLocation("Detroit"), Division(MLBPPDivision.ALCentral), LSTeamId(116)]
     Tigers,
-    [Abbrev("KC"), TeamLocation("Kansas City"), Division(MLBPPDivision.ALCentral)]
+    [Abbrev("KC"), TeamLocation("Kansas City"), Division(MLBPPDivision.ALCentral), LSTeamId(118)]
     Royals,
-    [Abbrev("Min"), TeamLocation("Minnesota"), Division(MLBPPDivision.ALCentral)]
+    [Abbrev("Min"), TeamLocation("Minnesota"), Division(MLBPPDivision.ALCentral), LSTeamId(142)]
     Twins,
-    [Abbrev("LAA"), TeamLocation("Anaheim"), Division(MLBPPDivision.ALWest)]
+    [Abbrev("LAA"), TeamLocation("Anaheim"), Division(MLBPPDivision.ALWest), LSTeamId(108)]
     Angels,
-    [Abbrev("Oak"), TeamLocation("Oakland"), Division(MLBPPDivision.ALWest)]
+    [Abbrev("Oak"), TeamLocation("Oakland"), Division(MLBPPDivision.ALWest), LSTeamId(133)]
     Athletics,
-    [Abbrev("Sea"), TeamLocation("Seattle"), Division(MLBPPDivision.ALWest)]
+    [Abbrev("Sea"), TeamLocation("Seattle"), Division(MLBPPDivision.ALWest), LSTeamId(136)]
     Mariners,
-    [Abbrev("TEX"), TeamLocation("Texas"), Division(MLBPPDivision.ALWest)]
+    [Abbrev("TEX"), TeamLocation("Texas"), Division(MLBPPDivision.ALWest), LSTeamId(140)]
     Rangers,
-    [Abbrev("Atl"), TeamLocation("Atlanta"), Division(MLBPPDivision.NLEast)]
+    [Abbrev("Atl"), TeamLocation("Atlanta"), Division(MLBPPDivision.NLEast), LSTeamId(144)]
     Braves,
-    [Abbrev("Fl"), TeamLocation("Florida"), Division(MLBPPDivision.NLEast)]
+    [Abbrev("Fl"), TeamLocation("Florida"), Division(MLBPPDivision.NLEast), LSTeamId(146)]
     Marlins,
-    [Abbrev("NYM"), TeamLocation("New York"), Division(MLBPPDivision.NLEast)]
+    [Abbrev("NYM"), TeamLocation("New York"), Division(MLBPPDivision.NLEast), LSTeamId(121)]
     Mets,
-    [Abbrev("Phi"), TeamLocation("Philadelphia"), Division(MLBPPDivision.NLEast)]
+    [Abbrev("Phi"), TeamLocation("Philadelphia"), Division(MLBPPDivision.NLEast), LSTeamId(143)]
     Phillies,
-    [Abbrev("WSH"), TeamLocation("Washington"), Division(MLBPPDivision.NLEast)]
+    [Abbrev("WSH"), TeamLocation("Washington"), Division(MLBPPDivision.NLEast), LSTeamId(120)]
     Nationals,
-    [Abbrev("CHC"), TeamLocation("Chicago"), Division(MLBPPDivision.NLCentral)]
+    [Abbrev("CHC"), TeamLocation("Chicago"), Division(MLBPPDivision.NLCentral), LSTeamId(112)]
     Cubs,
-    [Abbrev("Cin"), TeamLocation("Cincinnati"), Division(MLBPPDivision.NLCentral)]
+    [Abbrev("Cin"), TeamLocation("Cincinnati"), Division(MLBPPDivision.NLCentral), LSTeamId(113)]
     Reds,
-    [Abbrev("Hou"), TeamLocation("Houston"), Division(MLBPPDivision.NLCentral)]
+    [Abbrev("Hou"), TeamLocation("Houston"), Division(MLBPPDivision.NLCentral), LSTeamId(117)]
     Astros,
-    [Abbrev("Mil"), TeamLocation("Milwaukee"), Division(MLBPPDivision.NLCentral)]
+    [Abbrev("Mil"), TeamLocation("Milwaukee"), Division(MLBPPDivision.NLCentral), LSTeamId(158)]
     Brewers,
-    [Abbrev("Pit"), TeamLocation("Pittsburgh"), Division(MLBPPDivision.NLCentral)]
+    [Abbrev("Pit"), TeamLocation("Pittsburgh"), Division(MLBPPDivision.NLCentral), LSTeamId(134)]
     Pirates,
-    [Abbrev("STL"), TeamLocation("St. Louis"), Division(MLBPPDivision.NLCentral)]
+    [Abbrev("STL"), TeamLocation("St. Louis"), Division(MLBPPDivision.NLCentral), LSTeamId(138)]
     Cardinals,
-    [Abbrev("Ari"), TeamLocation("Arizona"), Division(MLBPPDivision.NLWest)]
+    [Abbrev("Ari"), TeamLocation("Arizona"), Division(MLBPPDivision.NLWest), LSTeamId(109)]
     DBacks,
-    [Abbrev("Col"), TeamLocation("Colorado"), Division(MLBPPDivision.NLWest)]
+    [Abbrev("Col"), TeamLocation("Colorado"), Division(MLBPPDivision.NLWest), LSTeamId(115)]
     Rockies,
-    [Abbrev("LAD"), TeamLocation("Los Angeles"), Division(MLBPPDivision.NLWest)]
+    [Abbrev("LAD"), TeamLocation("Los Angeles"), Division(MLBPPDivision.NLWest), LSTeamId(119)]
     Dodgers,
-    [Abbrev("SD"), TeamLocation("San Diego"), Division(MLBPPDivision.NLWest)]
+    [Abbrev("SD"), TeamLocation("San Diego"), Division(MLBPPDivision.NLWest), LSTeamId(135)]
     Padres,
-    [Abbrev("SF"), TeamLocation("San Francisco"), Division(MLBPPDivision.NLWest)]
+    [Abbrev("SF"), TeamLocation("San Francisco"), Division(MLBPPDivision.NLWest), LSTeamId(137)]
     Giants,
-    [Abbrev("AL"), DisplayName("American League All-Stars"), Division(MLBPPDivision.AllStars)]
+    [Abbrev("AL"), DisplayName("American League All-Stars"), Division(MLBPPDivision.AllStars), LSTeamId(159)]
     AmericanLeagueAllStars,
   }
 
@@ -124,6 +124,16 @@ namespace PowerUp.Entities
     public DivisionAttribute(MLBPPDivision division)
     {
       Division = division;
+    }
+  }
+
+  public class LSTeamIdAttribute : Attribute
+  {
+    public long LSTeamId { get; set; }
+
+    public LSTeamIdAttribute(long lsTeamId)
+    {
+      LSTeamId = lsTeamId;
     }
   }
 }
