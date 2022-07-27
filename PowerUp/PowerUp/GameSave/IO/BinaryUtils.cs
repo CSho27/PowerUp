361bb35b17653b68dea000b20d2bbb6c498ac09b
Chris Shorter <shorte24@gmail.com>
2022-07-26 20:28:20 -0400

    Adds isLittleEndian to BinaryUtils

--- PowerUp/PowerUp/GameSave/IO/BinaryUtils.cs
+++ PowerUp/PowerUp/GameSave/IO/BinaryUtils.cs
@@ -21,38 +21,46 @@ namespace PowerUp.GameSave.IO
 
     public static string ToBitString(this byte[] bytes, bool formatted = true) => string.Join(formatted ? " | " : "", bytes.Select(b => b.ToBitString(formatted)));
 
-    public static byte GetBit(this byte @byte, int position)
+    public static byte GetBit(this byte @byte, int position, bool isLittleEndian = false)
     {
-      var shift = BYTE_LENGTH - position - 1;
+      var shift = isLittleEndian
+        ? position
+        : BYTE_LENGTH - position - 1;
       return (byte)(@byte >> shift & 1);
     }
 
-    public static byte SetBit(this byte @byte, int position, byte newValue)
+    public static byte SetBit(this byte @byte, int position, byte newValue, bool isLittleEndian = false)
     {
       if (newValue != 0 && newValue != 1)
         throw new ArgumentException("New value can only be 0 or 1");
 
-      var shift = BYTE_LENGTH - position - 1;
+      var shift = isLittleEndian
+        ? position
+        : BYTE_LENGTH - position - 1;
       @byte ^= (byte)((-newValue ^ @byte) & 1 << shift);
       return @byte;
     }
 
-    public static ushort ToUInt16(this byte[] bits)
+    public static ushort ToUInt16(this byte[] bits, bool isLittleEndian = false)
     {
       if (bits.Length > 16)
         throw new ArgumentException("Number of bits exceeded 16 bit maximum");
 
+      var bitArray = isLittleEndian
+        ? bits.Reverse().ToArray()
+        : bits;
+
       var value = 0;
       var digits = 0;
-      for (int i = bits.Length - 1; i >= 0; i--)
+      for (int i = bitArray.Length - 1; i >= 0; i--)
       {
-        value += (int)Math.Pow(2, digits) * bits[i];
+        value += (int)Math.Pow(2, digits) * bitArray[i];
         digits++;
       }
       return (ushort)value;
     }
 
-    public static byte[] ToBitArray(this ushort @uint, int numberOfBits)
+    public static byte[] ToBitArray(this ushort @uint, int numberOfBits, bool isLittleEndian = false)
     {
       var bits = new byte[numberOfBits];
       var currentValue = 0;
@@ -72,7 +80,9 @@ namespace PowerUp.GameSave.IO
       if (currentValue < @uint)
         throw new ArgumentException("uint value is too large for the number of bits specified", nameof(@uint));
 
-      return bits;
+      return isLittleEndian
+        ? bits.Reverse().ToArray()
+        : bits;
     }
   }
 }
