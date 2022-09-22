f94ec26085946f7c42ca5f99fa8a9cc2e32f7cd8
Chris Shorter <shorte24@gmail.com>
2022-09-21 21:19:56 -0400

    Adds get n years before extension

--- PowerUp/PowerUp.Tests/Utils/DateExtensionsTests.cs
+++ PowerUp/PowerUp.Tests/Utils/DateExtensionsTests.cs
@@ -25,5 +25,25 @@ namespace PowerUp.Tests.Utils
       var result = firstDate.YearsElapsedSince(secondDate);
       result.ShouldBe(7);
     }
+
+    [Test]
+    public void GetDateNYearsBefore_GetsCorrectNumber_WhenDayOfYearHasNotPassed()
+    {
+      var firstDate = new DateTime(2007, 6, 1);
+      var secondDate = new DateTime(2000, 7, 1);
+
+      var result = firstDate.GetDateNYearsBefore(7, 1, 6);
+      result.ShouldBe(secondDate);
+    }
+
+    [Test]
+    public void GetDateNYearsBefore_GetsCorrectNumber_WhenDayOfYearHasPassed()
+    {
+      var firstDate = new DateTime(2007, 6, 1);
+      var secondDate = new DateTime(2000, 5, 1);
+
+      var result = firstDate.YearsElapsedSince(secondDate);
+      result.ShouldBe(7);
+    }
   }
 }
--- PowerUp/PowerUp/DateExtensions.cs
+++ PowerUp/PowerUp/DateExtensions.cs
@@ -8,5 +8,14 @@ namespace PowerUp
       firstDate.DayOfYear >= secondDate.DayOfYear
         ? firstDate.Year - secondDate.Year
         : firstDate.Year - secondDate.Year - 1;
+
+    public static DateTime GetDateNYearsBefore(this DateTime date, int month, int day, int yearsBefore)
+    {
+      if (date.Month > month && date.Day > day)
+        return new DateTime(date.Year-yearsBefore, month, day);
+      else
+        return new DateTime(date.Year-yearsBefore-1, month, day);
+    } 
+
   }
 }
