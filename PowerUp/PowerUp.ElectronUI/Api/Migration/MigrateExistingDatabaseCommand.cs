96c768a89ae9d5f8726e59939c349d3669f91a29
Chris Shorter <shorte24@gmail.com>
2022-10-02 20:58:42 -0400

    Creates command for migrating an existing database

---
+++ PowerUp/PowerUp.ElectronUI/Api/Migration/MigrateExistingDatabaseCommand.cs
@@ -0,0 +1,26 @@
+﻿using PowerUp.ElectronUI.Api.Shared;
+using PowerUp.Migrations;
+
+namespace PowerUp.ElectronUI.Api.Migration
+{
+  public class MigrateExistingDatabaseCommand : ICommand<MigrateExistingDatabaseRequest, ResultResponse>
+  {
+    private readonly IMigrationApi _migrationApi;
+
+    public MigrateExistingDatabaseCommand(IMigrationApi migrationApi)
+    {
+      _migrationApi = migrationApi;
+    }
+
+    public ResultResponse Execute(MigrateExistingDatabaseRequest request)
+    {
+      _migrationApi.MigrateDataFrom("./Data");
+      return ResultResponse.Succeeded();
+    }
+  }
+
+  public class MigrateExistingDatabaseRequest
+  {
+    public string? PowerUpDirectory { get; set; }
+  }
+}
