790875ed9b78532d664616e645d85f463dd71782
Chris Shorter <shorte24@gmail.com>
2022-11-03 22:24:58 -0400

    Removes other migration structure to commit to new simpler one

--- PowerUp/PowerUp/Migrations/MigrationApi.cs
+++ PowerUp/PowerUp/Migrations/MigrationApi.cs
@@ -1,11 +1,5 @@
 ﻿using PowerUp.Databases;
-using PowerUp.Migrations.MigrationTypes;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PowerUp.Migrations
 {
@@ -16,23 +10,6 @@ namespace PowerUp.Migrations
 
   public class MigrationApi : IMigrationApi
   {
-    public void MigrateDataFrom2(string dataDirectory)
-    {
-      using var existingDatabase = new EntityDatabase(dataDirectory);
-      foreach(var entityType in MigrationHelpers.GetAllEntityTypes())
-      {
-        var migrationType = MigrationHelpers.GetMigrationTypeFor(entityType)!;
-        var migrator = MigrationHelpers.GetMigratorFor(entityType, migrationType)!;
-        var allObjectsForEntity = existingDatabase
-          .LoadAll(migrationType)
-          .Select(o => migrator.Migrate(o))
-          .Cast<Entity>()
-          .Where(o => o is not null && !o.ShouldIgnoreInMigration);
-
-        DatabaseConfig.Database.SaveAll(entityType, allObjectsForEntity);
-      }
-    }
-
     public void MigrateDataFrom(string dataDirectory)
     {
       using var existingDatabase = new EntityDatabase(dataDirectory);
