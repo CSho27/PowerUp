70165e99afbe8a0cfd41ba2d9107e46fa9a3d926
Chris Shorter <shorte24@gmail.com>
2022-11-26 07:51:02 -0500

    Creates and tests GetNumberOfBytesNeeded on UIntInterpreter

---
+++ PowerUp/PowerUp.Tests/GameSave/IO/UIntInterpretTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using PowerUp.GameSave.IO;
+using Shouldly;
+
+namespace PowerUp.Tests.GameSave.IO
+{
+  public class UIntInterpretTests
+  {
+    [Test]
+    [TestCase(0, 4, 1)]
+    [TestCase(0, 8, 1)]
+    [TestCase(0, 16, 2)]
+    [TestCase(4, 4, 1)]
+    [TestCase(4, 8, 2)]
+    [TestCase(4, 16, 3)]
+    public void GetsCorrectNumberOfBytesToRead(int bitOffset, int numberOfBits, int expectedBytesToRead)
+    {
+      UIntInterpret.GetNumberOfBytesNeeded(bitOffset, numberOfBits).ShouldBe(expectedBytesToRead);
+    }
+  }
+}
---
+++ PowerUp/PowerUp/GameSave/IO/UIntInterpret.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace PowerUp.GameSave.IO
+{
+  public static class UIntInterpret
+  {
+    // We need the bytes for every bit touched by this uint, so if the divided value is a fraction, we always want to round up
+    public static int GetNumberOfBytesNeeded(int bitOffset, int numberOfBits) => (int)Math.Ceiling((bitOffset + numberOfBits) / (double)BinaryUtils.BYTE_LENGTH);
+  }
+}
