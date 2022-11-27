e5ff1a0efa61327bd1d1086575811ca0e17854da
Chris Shorter <shorte24@gmail.com>
2022-11-27 11:44:47 -0500

    Adds left wristband

--- PowerUp/PowerUp.Tests/GameSave/Objects/Players/Ps2PlayerReaderTests.cs
+++ PowerUp/PowerUp.Tests/GameSave/Objects/Players/Ps2PlayerReaderTests.cs
@@ -282,5 +282,17 @@ namespace PowerUp.Tests.GameSave.Objects.Players
       var player = loader.Read(playerId);
       player.RightWristband.ShouldBe(rightWristband);
     }
+
+    [Test]
+    [TestCase(JASON_GIAMBI_ID, (ushort)1)]
+    [TestCase(SAMMY_SPEEDSTER_ID, (ushort)5)]
+    [TestCase(PAUL_PITCHER_ID, (ushort)4)]
+    [TestCase(PETE_SALTINE_ID, (ushort)7)]
+    public void Reads_LeftWristband(int playerId, ushort leftWristband)
+    {
+      using var loader = new PlayerReader(_characterLibrary, TEST_READ_GAME_SAVE_FILE_PATH, GameSaveFormat.Ps2_2007);
+      var player = loader.Read(playerId);
+      player.LeftWristband.ShouldBe(leftWristband);
+    }
   }
 }
