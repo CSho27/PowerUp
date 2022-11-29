96298a9b156aa6873193ccb898ae68760921bc40
Chris Shorter <shorte24@gmail.com>
2022-11-29 06:28:05 -0500

    Adds GoodDelivery

--- PowerUp/PowerUp/GameSave/Objects/Players/Ps2GSPlayer.cs
+++ PowerUp/PowerUp/GameSave/Objects/Players/Ps2GSPlayer.cs
@@ -395,7 +395,7 @@ namespace PowerUp.GameSave.Objects.Players
     [GSBoolean(0x7f, bitOffset: 6)]
     public bool? GoodPickoff { get; set; }
 
-    [GSBoolean(0x7f, bitOffset: 2)]
+    [GSBoolean(0x7f, bitOffset: 5)]
     public bool? GoodDelivery { get; set; }
 
     [GSBoolean(0x7f, bitOffset: 3)]
