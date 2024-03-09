65f596c58f2bab1d08f3b01a3b0cbc88847d5a1c
Chris Shorter <shorte24@gmail.com>
2024-03-09 07:38:24 -0500

    Creates PlayerCsvWriter and tests

---
+++ PowerUp/PowerUp/CSV/PlayerCsvWriter.cs
@@ -0,0 +1,23 @@
+﻿using CsvHelper;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace PowerUp.CSV
+{
+  public interface IPlayerCsvWriter
+  {
+    public Task WriteAllPlayers(Stream stream, IEnumerable<CsvPlayer> players);
+  }
+
+  public class PlayerCsvWriter : IPlayerCsvWriter
+  {
+    public async Task WriteAllPlayers(Stream stream, IEnumerable<CsvPlayer> players)
+    {
+      using var writer = new StreamWriter(stream);
+      using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
+      await csv.WriteRecordsAsync(players);
+    }
+  }
+}
