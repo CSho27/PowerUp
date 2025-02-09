5bae7b71f0e6960c92a05504ed220ebf5df9a259
Chris Shorter <shorte24@gmail.com>
2025-02-09 06:25:19 -0500

    Sets up CommandLine project and adds Serilog to app

---
+++ PowerUp/PowerUp.CommandLine/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.CommandLine;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+
+var appHost = StartUp();
+await Run(appHost);
+
+async Task Run(IHost host)
+{
+  var quit = false;
+
+  await using var scope = host.Services.CreateAsyncScope();
+  //var commands = CommandRegistry.BuildRootCommand(scope.ServiceProvider, () => { quit = true; });
+
+  while (!quit)
+  {
+    Console.WriteLine();
+    Console.WriteLine("Enter command:");
+    Console.Write(">");
+    var command = Console.ReadLine();
+
+    if (!string.IsNullOrEmpty(command))
+    {
+      //await commands.InvokeAsync(command);
+    }
+  }
+}
+
+static IHost StartUp()
+{
+  var builder = Host.CreateDefaultBuilder();
+  /*
+  var projectRoot = FileSystemUtilities.GetProjectRootDirectory()!;
+  var configuration = new ConfigurationBuilder()
+      .SetBasePath(projectRoot)
+      .AddJsonFile("appsettings.json")
+      .Build();
+  var appSettingsProvider = new AppSettingsProvider(configuration);
+  */
+  var timestamp = DateTime.UtcNow.ToString("yyyyMMdd_HHmmss_ffffff");
+  Log.Logger = new LoggerConfiguration()
+      .MinimumLevel.Debug()
+      .WriteTo.Console(Serilog.Events.LogEventLevel.Information)
+      .WriteTo.File(Path.Combine(Directory.GetDirectoryRoot(Directory.GetCurrentDirectory()), "logs", $"log-{timestamp}.txt"), rollingInterval: RollingInterval.Infinite)
+      .CreateLogger();
+
+  builder.UseSerilog();
+  builder.ConfigureServices(services =>
+  {
+    services.AddLogging();
+  });
+
+  var host = builder.Build();
+  host.Start();
+  return host;
+}
