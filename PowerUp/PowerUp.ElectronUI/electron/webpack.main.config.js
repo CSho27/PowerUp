539ca848658c27afeaea840a73c4a31b559095b9
Chris Shorter <shorte24@gmail.com>
2025-02-24 22:20:51 -0500

    Attempts to add electron-forge to project (unsuccessfully)

--- PowerUp/PowerUp.ElectronUI/electron/webpack.main.config.js
+++ PowerUp/PowerUp.ElectronUI/electron/webpack.main.config.js
@@ -1,48 +1,34 @@
 const webpack = require('webpack');
 const path = require('path');
-
-module.exports = getConfig;
+const rules = require('./webpack.rules');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 process.loadEnvFile();
-function getConfig(env, argv) {
-  return {
-    mode: argv.mode,
-    target: "electron-renderer",
-    devtool: "source-map",
-    entry: './src/electronApp/main.tsx',
-    output: { 
-      filename: '[name].js',
-      path: path.resolve(__dirname, 'dist/main'),
-      clean: true,
-    },
-    watchOptions: {
-      ignored: ['**/node_modules/', '**/dist']
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(ts|tsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "ts-loader"
-          }
-        },
-        {
-          test: /\.css$/i,
-          use: ["style-loader", "css-loader"],
-        },
-      ]
-    },
-    resolve: {
-      extensions: [".ts", ".tsx", ".js"]
-    },
-    plugins: [
-      new webpack.DefinePlugin({
-        'process.env': JSON.stringify(process.env)
-      })
-    ],
-    node: {
-      __dirname: true
-    }
-  };
-}
+
+module.exports = {
+  target: "electron-renderer",
+  devtool: "source-map",
+  entry: './src/electronApp/main.tsx',
+  output: { 
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist/main'),
+    clean: true,
+  },
+  watchOptions: {
+    ignored: ['**/node_modules/', '**/dist']
+  },
+  module: {
+    rules: rules
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({
+      logger: 'webpack-infrastructure',
+    }),
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify(process.env)
+    })
+  ],
+};
--- PowerUp/PowerUp.ElectronUI/electron/webpack.renderer.config.js
+++ PowerUp/PowerUp.ElectronUI/electron/webpack.renderer.config.js
@@ -1,64 +1,53 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require('path');
+const rules = require('./webpack.rules');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
-module.exports = getConfig;
+const entries = {
+  preload: { entry: './src/electronApp/preload.tsx' },
+  renderer: { entry: './src/electronApp/renderer.tsx', html: true },
+  index: { entry: './src/webApp/renderer.tsx', html: true },
+};
 
-function getConfig(env, argv) {
-  const entries = {
-    preload: { entry: './src/electronApp/preload.tsx' },
-    renderer: { entry: './src/electronApp/renderer.tsx', html: true },
-    index: { entry: './src/webApp/renderer.tsx', html: true },
-  }
-
-  const htmlPlugins = Object.keys(entries).filter(name => !!entries[name].html).map(name => {
-    return new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: `./${name}.html`,
-      chunks: [name],
-    });
+const htmlPlugins = Object.keys(entries).filter(name => !!entries[name].html).map(name => {
+  return new HtmlWebPackPlugin({
+    template: "./src/index.html",
+    filename: `./${name}.html`,
+    chunks: [name],
   });
+});
 
-  const copyPatterns = [
-    { from: 'dist/renderer/index.js', to: '../../../wwwroot/index.js' },
-    { from: 'dist/renderer/index.html', to: '../../../wwwroot/index.html' }
-  ];
-  if(argv.mode === 'development') copyPatterns.push({ from: 'dist/renderer/index.js.map', to: '../../../wwwroot/index.js.map' },)
+const copyPatterns = [
+  { from: 'dist/renderer/index.js', to: '../../../wwwroot/index.js' },
+  { from: 'dist/renderer/index.html', to: '../../../wwwroot/index.html' },
+  { from: 'dist/renderer/index.js.map', to: '../../../wwwroot/index.js.map' },
+];
 
-  return {
-    mode: argv.mode,
-    target: "electron-renderer",
-    devtool: "source-map",
-    entry: Object.fromEntries(Object.entries(entries).map(([name, { entry }]) => [name, entry])),
-    output: { 
-      filename: '[name].js',
-      path: path.resolve(__dirname, 'dist/renderer'),
-      clean: true,
-    },
-    watchOptions: {
-      ignored: ['**/node_modules/', '**/dist']
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(ts|tsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "ts-loader"
-          }
-        },
-        {
-          test: /\.css$/i,
-          use: ["style-loader", "css-loader"],
-        },
-      ]
-    },
-    resolve: {
-      extensions: [".ts", ".tsx", ".js"]
-    },
-    plugins: [
-      ...htmlPlugins,
-      new CopyWebpackPlugin({ patterns: copyPatterns }),
-    ],
-  };
-}
+
+module.exports = {
+  target: "electron-renderer",
+  devtool: "source-map",
+  entry: Object.fromEntries(Object.entries(entries).map(([name, { entry }]) => [name, entry])),
+  output: { 
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist/renderer'),
+    clean: true,
+  },
+  watchOptions: {
+    ignored: ['**/node_modules/', '**/dist']
+  },
+  module: {
+    rules: rules,
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({
+      logger: 'webpack-infrastructure',
+    }),
+    ...htmlPlugins,
+    new CopyWebpackPlugin({ patterns: copyPatterns }),
+  ],
+};
