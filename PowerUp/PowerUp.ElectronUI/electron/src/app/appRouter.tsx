80ff3257a039221856a478ef6f8dc3aa19a27300
Chris Shorter <shorte24@gmail.com>
2025-03-02 08:10:10 -0500

    Updates app to use createBrowserRouter

--- PowerUp/PowerUp.ElectronUI/electron/src/app/appRouter.tsx
+++ PowerUp/PowerUp.ElectronUI/electron/src/app/appRouter.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Outlet, Params, Route, Routes, useNavigate, useParams } from "react-router-dom";
+import { BrowserRouter, createBrowserRouter, createRoutesFromChildren, Outlet, Params, Route, RouterProvider, Routes, useNavigate, useParams } from "react-router-dom";
 import { useAppContext } from "./appContext";
 import { HomePage, loadHomePageProps } from "./home/homePage";
 import { ReactNode } from "react";
@@ -10,64 +10,64 @@ import { loadTeamEditorPageProps, TeamEditorPage } from "./teamEditor/teamEditor
 import { DraftPage, loadDraftPageProps } from "./draftPage/draftPage";
 import { Page } from "./app";
 
+const router = createBrowserRouter(createRoutesFromChildren(<>
+  <Route 
+    path='' 
+    element={<Page>
+      <Outlet />
+    </Page>}
+  >
+    <Route 
+      index
+      element={<PageLoader 
+        pageDef={() => ({ page: 'HomePage' })} 
+        loadProps={loadHomePageProps} 
+        renderPage={props => <HomePage {...props} />}
+      />} 
+    />
+    <Route 
+      path='roster/:rosterId' 
+      element={<PageLoader 
+        pageDef={p => ({ page: 'RosterEditorPage', rosterId: Number.parseInt(p.rosterId ?? '') })} 
+        loadProps={loadRosterEditorPageProps} 
+        renderPage={props => <RosterEditorPage {...props} />}
+      />} 
+    />
+    <Route 
+      path='team/:teamId' 
+      element={<PageLoader 
+        pageDef={p => ({ 
+          page: 'TeamEditorPage', 
+          teamId: Number.parseInt(p.teamId ?? ''), 
+          tempTeamId: p.tempTeamId
+            ? Number.parseInt(p.tempTeamId)
+            : undefined  
+        })} 
+        loadProps={loadTeamEditorPageProps} 
+        renderPage={props => <TeamEditorPage {...props} />}
+      />} 
+    />
+    <Route 
+      path='player/:playerId' 
+      element={<PageLoader 
+        pageDef={p => ({ page: 'PlayerEditorPage', playerId: Number.parseInt(p.playerId ?? '') })} 
+        loadProps={loadPlayerEditorPageProps} 
+        renderPage={props => <PlayerEditorPage {...props} />}
+      />}
+    />
+    <Route 
+      path='draft/:rosterId' 
+      element={<PageLoader 
+        pageDef={p => ({ page: 'DraftPage', rosterId: Number.parseInt(p.rosterId ?? '') })} 
+        loadProps={loadDraftPageProps} 
+        renderPage={props => <DraftPage {...props} />}
+      />}
+    />
+  </Route>
+</>));
+
 export function AppRouter() {
-  return <BrowserRouter>
-    <Routes>
-      <Route 
-        path='' 
-        element={<Page>
-          <Outlet />
-        </Page>}
-      >
-        <Route 
-          index
-          element={<PageLoader 
-            pageDef={() => ({ page: 'HomePage' })} 
-            loadProps={loadHomePageProps} 
-            renderPage={props => <HomePage {...props} />}
-          />} 
-        />
-        <Route 
-          path='roster/:rosterId' 
-          element={<PageLoader 
-            pageDef={p => ({ page: 'RosterEditorPage', rosterId: Number.parseInt(p.rosterId ?? '') })} 
-            loadProps={loadRosterEditorPageProps} 
-            renderPage={props => <RosterEditorPage {...props} />}
-          />} 
-        />
-        <Route 
-          path='team/:teamId' 
-          element={<PageLoader 
-            pageDef={p => ({ 
-              page: 'TeamEditorPage', 
-              teamId: Number.parseInt(p.teamId ?? ''), 
-              tempTeamId: p.tempTeamId
-                ? Number.parseInt(p.tempTeamId)
-                : undefined  
-            })} 
-            loadProps={loadTeamEditorPageProps} 
-            renderPage={props => <TeamEditorPage {...props} />}
-          />} 
-        />
-        <Route 
-          path='player/:playerId' 
-          element={<PageLoader 
-            pageDef={p => ({ page: 'PlayerEditorPage', playerId: Number.parseInt(p.playerId ?? '') })} 
-            loadProps={loadPlayerEditorPageProps} 
-            renderPage={props => <PlayerEditorPage {...props} />}
-          />}
-        />
-        <Route 
-          path='draft/:rosterId' 
-          element={<PageLoader 
-            pageDef={p => ({ page: 'DraftPage', rosterId: Number.parseInt(p.rosterId ?? '') })} 
-            loadProps={loadDraftPageProps} 
-            renderPage={props => <DraftPage {...props} />}
-          />}
-        />
-      </Route>
-    </Routes>
-  </BrowserRouter>
+  return <RouterProvider router={router} /> 
 }
 
 interface PageLoaderProps<TPageProps> {
